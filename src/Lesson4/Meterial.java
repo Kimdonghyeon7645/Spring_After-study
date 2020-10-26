2a9fb550af637236ae7f117084688f51812de0ac
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-10-26 20:16:51 +0900

    docs : edit Meterial

--- src/Lesson4/Meterial.java
+++ src/Lesson4/Meterial.java
@@ -1,11 +1,5 @@
 package Lesson4;
 
-public class Meterial {
-    @Override
-    public String toString() {
-        return super.toString();
-    }
-    public void doPrinting() {
-        System.out.println("Powder로 프린팅 합니다.");
-    }
+public abstract class Meterial {
+    public abstract void doPrinting();
 }
